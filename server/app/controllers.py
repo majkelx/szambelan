c02a82ab5789734bc605e94b9963b1fd14172f1d
majkelx <majkelx.github@2007.gfdgfdg.com>
2016-04-12 13:53:19 +0200

    500 limit, list title, all fields displayed, dict iteration

--- server/app/controllers.py
+++ server/app/controllers.py
@@ -3,9 +3,12 @@ import cherrypy
 
 class HelloWorld(object):
     def index(self):
-        ret = '<html><body><table style="width:100%">'
+        ret = '<html><body><h1>Last 500 logs</h1><table style="width:100%">'
         for log in self.logs:
-            ret += '<tr><td>' + log['ts'] + '</td><td>' + log['fid'] + '</td><td>' + log['raw'] + '</td></tr>'
+            ret += '<tr>'
+            for fld in log.values():
+                ret += '<td>' + fld + '</td>'
+            ret += '</tr>'
         ret += '</table></body></html>'
         return ret
     index.exposed = True
--- server/app/ctrl_log.py
+++ server/app/ctrl_log.py
@@ -13,7 +13,11 @@ class LogService(object):
     def POST(self):
         log =  cherrypy.request.json
         for frame in log['frames']:
-            self.logs.append(frame)
+            frame['ver'] = log['ver']
+            frame['dev'] = log['dev']
+            self.logs.insert(0, frame)
+            if len(self.logs) > 500:
+                self.logs.pop()
         print (log)
 
 #     def PUT(self, another_string):
